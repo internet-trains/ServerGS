aec816178fc60dc837f9d7e4cffde92db3a2d41f
Ian Earle <ian.s.m.earle@gmail.com>
2020-08-28 22:46:53 -0300

    Update gen_api_binding.py
    Updating for py3 & dev enviroment (will remove hard coding soon i promise)

--- gen_api_binding.py
+++ gen_api_binding.py
@@ -1,41 +1,41 @@
-#!python
-
 import glob
 import os
 import re
 from subprocess import Popen,PIPE
 
 # == Settings ==
-openttd_path = "C:\Users\Leif\Programmering\OpenTTD\hg_plain_trunk"
-gs_path = "src/script/api/game"
+openttd_path = "../.."
+gs_path = "../../../src/script/api/game"
 # ==============
 
 # Open output file before changing current path
-f_out = open("symbols.nut", "w");
+f_out = open("symbols.nut", "w")
 
 # -- Get OpenTTD version
-os.chdir(openttd_path);
+os.chdir(openttd_path)
 # If hg queues are used, the svn version is given below all patches, therefore ask for 100 log messages.
-changelog = Popen("hg log --limit 100", stdout=PIPE, shell=True).stdout.read();
-version = None
+# changelog = Popen("hg log --limit 100", stdout=PIPE, shell=True).stdout.read();
+# version = None
 
-for line in changelog.split("\n"):
-	r = re.search("\\(svn (r[0-9]+)\\)", line)
-	if r != None:
-		version = r.group(1)
-		break
+# for line in changelog.split("\n"):
+# 	r = re.search("\\(svn (r[0-9]+)\\)", line)
+# 	if r != None:
+# 		version = r.group(1)
+# 		break
+
+version = "(TODO)"
 
 # -- Print header in output .nut file
 f_out.write("/*\n")
 f_out.write(" * This file has been generated from OpenTTD source code (" + version + "),\n")
-f_out.write(" * so please don't edit it manually. Instead, re-generate it using\n");
-f_out.write(" * gen_api_binding.py\n");
+f_out.write(" * so please don't edit it manually. Instead, re-generate it using\n")
+f_out.write(" * gen_api_binding.py\n")
 f_out.write(" */\n")
-f_out.write("g_symbols_version <- \"" + version + "\";\n");
-f_out.write("g_symbols <- [\n");
+f_out.write("g_symbols_version <- \"" + version + "\";\n")
+f_out.write("g_symbols <- [\n")
 
 # -- Scan all .sq files for the GS API in the OpenTTD source code
-os.chdir(openttd_path + "/" + gs_path);
+os.chdir(openttd_path + "/" + gs_path)
 for file_name in glob.glob("*.sq"):
 
 	class_name = None
@@ -45,26 +45,26 @@ for file_name in glob.glob("*.sq"):
 
 		# Look for class name?
 		if class_name == None:
-			r = re.search("DefSQClass.*\\(\"(.*)\"\\);", line);
+			r = re.search("DefSQClass.*\\(\"(.*)\"\\);", line)
 			if r != None:
-				class_name = r.group(1);
+				class_name = r.group(1)
 
 				# Open class array and give class name in first element
 				# also open array for members
-				f_out.write("\t[\"" + class_name + "\", [\n");
+				f_out.write("\t[\"" + class_name + "\", [\n")
 
 		else:
 			# Member - const?
-			r = re.search("DefSQConst.*\"(.*)\"\\);", line);
+			r = re.search("DefSQConst.*\"(.*)\"\\);", line)
 			has_arg_types = False
 			if r == None:
 				# or method?
-				r = re.search("DefSQStaticMethod\\(.*\"(.*)\",.*\"\\.(.*)\"", line);
+				r = re.search("DefSQStaticMethod\\(.*\"(.*)\",.*\"\\.(.*)\"", line)
 				if r != None:
 					has_arg_types = True # second group contains arg types
 			if r == None:
 				# or advanced method?
-				r = re.search("DefSQAdvancedStaticMethod.*\"(.*)\"\\);", line);
+				r = re.search("DefSQAdvancedStaticMethod.*\"(.*)\"\\);", line)
 			if r != None:
 				member_name = r.group(1)
 				if has_arg_types:
@@ -83,13 +83,13 @@ for file_name in glob.glob("*.sq"):
 			if re.search("^}$", line.strip()) != None:
 
 				if has_no_member:
-					f_out.write("\t\t[\"\", " + class_name + "],\n");
+					f_out.write("\t\t[\"\", " + class_name + "],\n")
 
 
 				f_out.write("\t]],\n") # terminate member list + class info
 
 				# Allow finding additional classes in the same file
-				class_name = None;
+				class_name = None
 
 
 	# End of .sq file
@@ -102,5 +102,4 @@ f_out.write("];\n")
 # -- Close output file
 f_out.close()
 
-print "symbols.nut has been re-generated based on OpenTTD " + version
-
+print("symbols.nut has been re-generated based on OpenTTD " + version)
